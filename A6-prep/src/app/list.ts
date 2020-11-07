bba8546b217ab8e7edd2d4391fbd6e225ea2e492
Carolista <cjones.renaissancewoman@gmail.com>
2020-11-06 23:32:12 -0600

    added List class

---
+++ A6-prep/src/app/list.ts
@@ -0,0 +1,16 @@
+export class List {
+
+    title: string;
+    desc: string;
+    items: string[];
+
+    constructor(title: string, desc: string) {
+        this.title = title;
+        this.desc = desc;
+        this.items = []; // always initialized to empty array
+    }
+
+    // TODO: write search method?
+
+    // TODO: write sort method?
+}
